d383710bbca61dff0cf2945d12124b44ffe08f47
KrolikowskiK <kacperk7373@gmail.com>
2021-11-29 21:32:01 +0100

    Refactored SignIn form

--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import { MyTextInput } from "../components/FormComponents";
+import * as Yup from "yup";
 import useAuth from "../components/Auth";
 import { useLocation, useNavigate } from "react-router";
 import * as css from "../styles/signIn.module.scss";
@@ -8,24 +11,57 @@ export default function SignIn() {
   const { signin, authed } = useAuth();
   const { state } = useLocation();
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    signin().then(() => {
-      const path = state === null ? "/" : state.path;
-      navigate(path);
-    });
-  }
-
   return authed ? (
     <h2 className={css.h2}>Już jesteś zalogowany</h2>
   ) : (
-    <form className={css.signin}>
+    <>
       <h1 className={css.header}>Logowanie</h1>
-      <input className={css.input} type="text" placeholder="E-mail" />
-      <input className={css.input} type="text" placeholder="Hasło" />
-      <button type="submit" className={css.button} onClick={handleSubmit}>
-        Zaloguj się
-      </button>
-    </form>
+      <Formik
+        initialValues={{
+          email: "",
+          pwd: "",
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string().required("Required"),
+          pwd: Yup.string().required("Required"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            signin().then(() => {
+              const path = state === null ? "/" : state.path;
+              navigate(path);
+            });
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form noValidate className={css.form}>
+          <MyTextInput
+            id="email"
+            name="email"
+            type="text"
+            label="E-mail"
+            placeholder="E-mail"
+            labelclass={css.label}
+            inputclass={css.input}
+            errorclass={css.error}
+          />
+          <MyTextInput
+            id="pwd"
+            name="pwd"
+            type="text"
+            label="Hasło"
+            placeholder="Hasło"
+            labelclass={css.label}
+            inputclass={css.input}
+            errorclass={css.error}
+          />
+          <button type="submit" className={css.button}>
+            Zaloguj się
+          </button>
+        </Form>
+      </Formik>
+    </>
   );
 }
