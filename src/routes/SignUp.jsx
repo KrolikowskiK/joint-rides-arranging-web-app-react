9cfed47edaef609f53c7420537f1496e6e95b4b7
KrolikowskiK <kacperk7373@gmail.com>
2021-11-29 21:14:25 +0100

    Refactored SignUp form

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import { MyTextInput } from "../components/FormComponents";
+import * as Yup from "yup";
 import useAuth from "../components/Auth";
 import * as css from "../styles/signUp.module.scss";
 
@@ -8,16 +11,74 @@ export default function SignUp() {
   return authed ? (
     <h2 className={css.h2}>Już jesteś zalogowany</h2>
   ) : (
-    <form className={css.signup}>
+    <>
       <h1 className={css.header}>Rejestracja</h1>
-      <input className={css.input} type="text" placeholder="Imię" />
-      <input className={css.input} type="text" placeholder="Nazwisko" />
-      <input className={css.input} type="text" placeholder="E-mail" />
-      <input className={css.input} type="text" placeholder="Hasło" />
-      <input className={css.input} type="text" placeholder="Powtórz hasło" />
-      <button type="submit" className={css.button}>
-        Zarejestruj się
-      </button>
-    </form>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          pwd: "",
+          pwdRe: "",
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required("Required"),
+          email: Yup.string().required("Required"),
+          pwd: Yup.string().required("Required"),
+          pwdRe: Yup.string().required("Required"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form className={css.form}>
+          <MyTextInput
+            id="name"
+            name="name"
+            type="text"
+            label="Imię"
+            placeholder="Imię"
+            labelclass={css.label}
+            inputclass={css.input}
+            errorclass={css.error}
+          />
+          <MyTextInput
+            id="email"
+            name="email"
+            type="text"
+            label="E-mail"
+            placeholder="E-mail"
+            labelclass={css.label}
+            inputclass={css.input}
+            errorclass={css.error}
+          />
+          <MyTextInput
+            id="pwd"
+            name="pwd"
+            type="text"
+            label="Hasło"
+            placeholder="Hasło"
+            labelclass={css.label}
+            inputclass={css.input}
+            errorclass={css.error}
+          />
+          <MyTextInput
+            id="pwdRe"
+            name="pwdRe"
+            type="text"
+            label="Powtórz hasło"
+            placeholder="Powtórz hasło"
+            labelclass={css.label}
+            inputclass={css.input}
+            errorclass={css.error}
+          />
+          <button type="submit" className={css.button}>
+            Zarejestruj się
+          </button>
+        </Form>
+      </Formik>
+    </>
   );
 }
