953038cc4751c657c0ef14dcb6ae653f2cc5b556
KrolikowskiK <kacperk7373@gmail.com>
2021-12-16 14:12:58 +0100

    feat: add loading animation component

---
+++ src/components/LoadingAnimation/LoadingAnimation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as css from "./style.module.scss";
+
+const LoadingAnimation = () => {
+  return <div className={css.animation}></div>;
+};
+
+export default LoadingAnimation;
