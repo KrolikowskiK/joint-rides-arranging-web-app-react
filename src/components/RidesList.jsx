321fa78b63566ea1645184f39fcf58c7a19e06bb
KrolikowskiK <kacperk7373@gmail.com>
2021-12-17 23:49:24 +0100

    feat: make RidesList show user's rides

--- src/components/RidesList.jsx
+++ src/components/RidesList.jsx
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RideCard from "./RideCard";
+import useCustomKyApi from "../components/KyApi";
 import * as css from "../styles/ridesList.module.scss";
-import data from "../data.json";
+import { formatDate } from "../utils";
+import LoadingAnimation from "./LoadingAnimation/LoadingAnimation";
+import Popup from "./Popup/Popup";
 
-export default function RidesList() {
-  let ridesDetails = data.rides;
+const RidesList = () => {
+  const api = useCustomKyApi();
+  const [rideCards, setRideCards] = useState();
+  const [popup, setPopup] = useState(null);
 
-  let rideCards = [];
-  for (let i = 0; i < 4; i++) {
-    rideCards.push(
-      <RideCard key={ridesDetails[i].id} rideDetails={ridesDetails[i]} />
-    );
-  }
+  useEffect(async () => {
+    try {
+      const ridesDetails = await api
+        .get("https://travelapi-app.azurewebsites.net/api/Trips/myTrips")
+        .json();
 
-  return <div className={css.ridesList}>{rideCards}</div>;
-}
+      setRideCards(
+        ridesDetails.map((details) => {
+          const rideDetails = {
+            id: details.id,
+            startDest: details.startFrom,
+            endDest: details.endIn,
+            startDateAndTime: formatDate(new Date(details.startTime)),
+            driver: details.creator.name,
+            price: details.price,
+          };
+          return <RideCard key={rideDetails.id} rideDetails={rideDetails} />;
+        })
+      );
+    } catch (error) {
+      setPopup(
+        <Popup
+          message="Nie udało się pobrać przejazdów"
+          onAnimationEnd={() => {
+            setPopup(null);
+          }}
+        />
+      );
+      setRideCards(null);
+    }
+  }, []);
+
+  return rideCards === undefined ? (
+    <LoadingAnimation />
+  ) : rideCards === null ? (
+    popup
+  ) : rideCards.length > 0 ? (
+    <div className={css.ridesList}>{rideCards}</div>
+  ) : (
+    <h2 className={css.header}>Brak przejazdów</h2>
+  );
+};
+
+export default RidesList;
