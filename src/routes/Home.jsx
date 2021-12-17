1dfcc49b327af31a9094134e84d5e1b39d8f0ef5
KrolikowskiK <kacperk7373@gmail.com>
2021-12-17 19:36:27 +0100

    feat: change start page to show all future rides
    Removed usage of RidesList as it shows user's rides.
    Added Popup indication when fetching fails.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RidesSearchForm from "../components/RidesSearchForm";
 import RidesListHeader from "../components/RidesListHeader";
-import RidesList from "../components/RidesList";
+import LoadingAnimation from "../components/LoadingAnimation/LoadingAnimation";
+import ky from "ky";
+import RideCard from "../components/RideCard";
+import { formatDate } from "../utils";
+import Popup from "../components/Popup/Popup";
 
 export default function Home() {
+  const [rideCards, setRideCards] = useState([]);
+  const [popup, setPopup] = useState(null);
+
+  useEffect(async () => {
+    try {
+      const rides = await ky
+        .get("https://travelapi-app.azurewebsites.net/api/Trips")
+        .json();
+
+      setRideCards(
+        rides.map((rideDetails) => {
+          const rides = {
+            id: rideDetails.id,
+            startDest: rideDetails.startFrom,
+            endDest: rideDetails.endIn,
+            startDateAndTime: formatDate(new Date(rideDetails.startTime)),
+            driver: rideDetails.creator.name,
+            price: rideDetails.price,
+          };
+          return <RideCard key={rides.id} rideDetails={rides} />;
+        })
+      );
+    } catch (error) {
+      setPopup(
+        <Popup
+          message="Nie udało się pobrać ostatnich przejazdów"
+          onAnimationEnd={() => {
+            setPopup(null);
+          }}
+        />
+      );
+    }
+  }, []);
+
   return (
     <>
       <RidesSearchForm />
       <RidesListHeader type="last" />
-      <RidesList />
+      {rideCards.length > 0 ? rideCards : <LoadingAnimation />}
+      {popup}
     </>
   );
 }
